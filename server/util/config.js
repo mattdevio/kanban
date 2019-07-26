77791a368aedb1aac39d966e9ffbdf008bd76f7a
Matt Greenberg <magreenberg@fullsail.edu>
2019-07-25 22:21:50 -0600

    feat: Added server start file and a config utility that loads the environment variables

---
+++ server/util/config.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+/**
+ * *config
+ * 
+ * @description Load the env variables and provides some config related helper functions
+ * @returns object
+ */
+const result = dotenv.config({
+  path: path.resolve(__dirname, '../../.env'),
+});
+
+if (result.error) {
+  throw result.error;
+}
+
+const config = Object.assign({}, result.parsed, {
+  isDevelopment: () => result.parsed.NODE_ENV === 'development',
+  isProduction: () => result.parsed.NODE_ENV === 'production',
+});
+
+module.exports = config;
