7e5de192b5559ff978e6cf6f99783cc56994d511
Matt Greenberg <magreenberg@fullsail.edu>
2019-07-25 23:15:13 -0600

    feat: the root now starts the kanban express app

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
 const config = require('./util/config');
+const kanban = require('./lib/kanban');
+const logger = require('./util/logger');
+
+kanban()
+  .listen(config.env.PORT, () => {
+    logger.info(`Server started: listening on port ${config.env.PORT}`);
+  });
